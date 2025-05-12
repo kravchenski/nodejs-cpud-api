994bb14cc35d80f6b1949976f3cc51b15b7b67e1
kravchenski <max5045955@gmail.com>
2025-05-12 23:54:58 +0200

    init: create a configuration file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import { resolve as _resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    entry: './src/index.ts',
+    target: 'node',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: _resolve(__dirname, 'dist'),
+    },
+    externals: {},
+};
